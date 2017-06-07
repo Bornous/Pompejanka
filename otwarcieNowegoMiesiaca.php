b9bc306f27862faede9717578c01fcfb8aa0ce28
Bornous <pavelgra77@gmail.com>
2017-06-07 20:29:21 +0200

    poprawione dodawanie miesiaca do danych

--- otwarcieNowegoMiesiaca.php
+++ otwarcieNowegoMiesiaca.php
@@ -30,7 +30,7 @@
 		else
 			{ 
 					echo "Udalo sie polaczy z baza";
-					if( $wczytaneDane = $bazaDanych->query("SELECT * FROM zapisy WHERE dataMonth='".$_SESSION['actualMonth']."' ") ){
+					if( $wczytaneDane = $bazaDanych->query("SELECT * FROM zapisy ") ){
 						$iloscWynikow=$wczytaneDane->num_rows;
 						if($iloscWynikow%54==0)	$dzienPompejanki=1;
 						else										$dzienPompejanki=$iloscWynikow+1;
@@ -42,7 +42,8 @@
 					$sukcesy=0;
 					for( $i=1;$i<=$liczba_dni_w_mies;$i++){
 						echo " ".$i." ".$dzienPompejanki;
-						if($bazaDanych->query("INSERT INTO zapisy ( dataDay, dataMonth, dataYear , imie, nazwisko, email, numerPomp) VALUES ('".$i."', '".$nowyMiesiac."', '".$year."' , ' ' , ' ', ' ' , '".$dzienPompejanki."' ,)") ){
+						$zapytanie="INSERT INTO zapisy ( dataDay, dataMonth, dataYear , imie, nazwisko, email, numerPomp) VALUES ('".$i."', '".$nowyMiesiac."', '".$year."' , ' ' , ' ', ' ' , '".$dzienPompejanki."' )";
+						if($bazaDanych->query($zapytanie) ){
 							$sukcesy++;
 						}
 						
@@ -51,7 +52,7 @@
 						
 					}
 				
-					echo " . Pomyślnie zaaplikowano ".$sukcesy." rekordów. NOwyM=".$nowyMiesiac." a year to ".$year;
+					echo " . Pomyślnie zaaplikowano ".$sukcesy." rekordów. NOwyM=".$nowyMiesiac." a year to ".$year." liczba dni w mies ".$liczba_dni_w_mies;
 					
 
 				$bazaDanych->close();	
